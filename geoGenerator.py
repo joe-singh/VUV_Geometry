1379b1e2a73b219a0c043321cd2e57424738befa
Joe Singh <joesingh@nino.physics.berkeley.edu>
2018-06-08 16:17:17 -0700

    Changed a few things in photon tracking

--- geoGenerator.py
+++ geoGenerator.py
@@ -11,18 +11,19 @@ world.invisible = 1
 masterString = world.writeToString(masterString)
 
 # Create outside cube
-cube = GS.BoxVolume('cube', 1, 1, 1)
-cube.material = 'pmt_vacuum'
-cube.mother = 'world'
-cube.colorVect[3] = 1.0
-cube.rotation = [45,0,0]
-masterString = cube.writeToString(masterString)
+#cube = GS.BoxVolume('cube', 1, 1, 1)
+#cube.material = 'pmt_vacuum'
+#cube.mother = 'world'
+#cube.colorVect[3] = 1.0
+#cube.rotation = [45,0,0]
+#masterString = cube.writeToString(masterString)
 
 # Create cylinder
-#cylinder = GS.TubeVolume('cylinder', 5, 5, 0)
-#cylinder.material = 'acrylic_black'
-#cylinder.mother = 'cube'
-#masterString = cylinder.writeToString(masterString)
+cylinder = GS.TubeVolume('cylinder', 1, 1, 0)
+cylinder.material = 'acrylic_black'
+cylinder.mother = 'world'
+cylinder.colorVect[3] = 1.0
+masterString = cylinder.writeToString(masterString)
 
 out_file = open(file_name, 'w+')
 out_file.write(masterString)
--- photonTracking.py
+++ photonTracking.py
@@ -10,7 +10,7 @@ inf = sys.argv[1]
 
 def track_photon(infile):
 
-    file_iterator = rat.dsReader(infile)
+    file_iterator = rat.dsreader(infile)
     tracking_dict = {}
 
     for iEntry, anEntry in enumerate(file_iterator):
@@ -20,19 +20,25 @@ def track_photon(infile):
         tracking_dict[iEntry] = []
         tempMC = anEntry.GetMC()
         num_tracks = tempMC.GetMCTrackCount()
+#        print("Tracks: ", num_tracks)
 
         photon_track = tempMC.GetMCTrack(0)
         photon_first_step = photon_track.GetMCTrackStep(0)
-
+         
         for iTrack in range(num_tracks):
             tempTrack = tempMC.GetMCTrack(iTrack)
+ #           print("Steps: ", tempTrack.GetMCTrackStepCount())
             for iStep in range(tempTrack.GetMCTrackStepCount()):
+                if iStep == 0:
+		    continue
                 step = tempTrack.GetMCTrackStep(iStep)
                 track_end_point = step.GetEndpoint()
                 xPos = track_end_point.x()
                 yPos = track_end_point.y()
                 zPos = track_end_point.z()
                 tracking_dict[iEntry] += [(xPos, yPos, zPos)]
+    
+    print(tracking_dict) 
 
     for particle in tracking_dict:
         coordinates = tracking_dict[particle]
@@ -41,15 +47,21 @@ def track_photon(infile):
         z_coordinates = [coordinate[2] for coordinate in coordinates]
         tracking_dict[particle] = [x_coordinates, y_coordinates, z_coordinates]
 
-    particle_number = random.randint(0, len(tracking_dict))
-    x_array = np.array(tracking_dict[particle_number][0])
-    y_array = np.array(tracking_dict[particle_number][1])
-    z_array = np.array(tracking_dict[particle_number][2])
-
     fig = plt.figure()
     ax = Axes3D(fig)
 
-    ax.scatter(x_array, y_array, z_array)
+#    ax.set_xlim3d(-10, 10)
+#    ax.set_ylim3d(-10,10)
+#    ax.set_zlim3d(-10,10)
+    # plot 10 random particles
+    for i in range(2000):
+
+        particle_number = random.randint(0, len(tracking_dict)-1)
+        x_array = np.array(tracking_dict[particle_number][0])
+        y_array = np.array(tracking_dict[particle_number][1])
+        z_array = np.array(tracking_dict[particle_number][2])
+        ax.scatter(x_array, y_array, z_array, c = 'blue', s=0.5)
+    
     plt.show()
 
 track_photon(inf)

b8cd54db791735bbce31ab8710b259dc42a507b1
Michinari Sakai <michsakai@gmail.com>
2019-04-03 13:46:51 -0700

    Make cluster_zero_sweep.sh run faster by only looking at +/- 5 degree from nominal reflectance peak.

--- cluster_zero_sweep.sh
+++ cluster_zero_sweep.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-if [ ! -d "./angle_sweep" ]; then 
-    mkdir ./angle_sweep
+### Make sure output directory exists
+if [ ! -d "angle_sweep" ]; then 
+    mkdir angle_sweep
 fi
+rm -r angle_sweep/* # Clean it
 
-s="85"
-while [ $s -lt 90 ]
-do
-    if [ ! -d "./angle_sweep/angle_$s" ]; then
-        mkdir ./angle_sweep/angle_$s
+### Make sure directory with macros exists
+if [ ! -d "macros" ]; then 
+    mkdir macros
+fi
+rm -r macros/* # Clean it
+
+s_min=215
+s_max=240
+s=$s_min
+while [ $s -le $s_max ]; do
+    if [ ! -d "angle_sweep/angle_$s" ]; then
+        mkdir angle_sweep/angle_$s
     fi
-    n="0" 
-    while [ $n -lt 95 ]
+
+	n_min=$((s - 5))
+	n_max=$((s + 5))
+    n=$n_min
+    while [ $n -le $n_max ]
     do
+		#if [ $n -ne 85 ]; then
+		#	n=$[n+5]
+		#	continue
+		#fi
         srun -p debug python geoGenMain.py $s $n 1.0
         srun -p debug python makePhotonMacro.py $s $n 400.0
-    echo ">>> Done $s $n"
-    srun -p debug --comment="$@VUVSim_$s_$n" rat -o ./angle_sweep/angle_$s/no_cylinder_angle_$s.$n.root ./macros/photon_$s.$n.mac $@ >/dev/null 2>&1 &
-    n=$[n+5]
+		echo ">>> Done $s $n"
+    	srun -p debug --comment="$@VUVSim_$s_$n" rat -o ./angle_sweep/angle_$s/no_cylinder_angle_$s.$n.root ./macros/photon_$s.$n.mac $@ >/dev/null 2>&1 &
+		n=$((n + 1))
     done 
-s=$[s+10]
+	s=$((s + 1))
 done

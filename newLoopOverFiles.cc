73f0340d2b806f561b6bebca798e632206a78e01
Michinari Sakai <michsakai@gmail.com>
2019-04-09 18:16:30 -0700

    Edit newLoopOverFiles.cc to also plot peak height vs incidence angle.

--- newLoopOverFiles.cc
+++ newLoopOverFiles.cc
@@ -1,6 +1,9 @@
 # include <iostream>
 # include <sstream>
 # include <exception>
+# include <math.h>
+# include <vector>
+# include <algorithm>
 # include <TROOT.h>
 # include <TFile.h>
 # include <TTree.h>
@@ -16,6 +19,8 @@
 # include <TMultiGraph.h>
 # include <TLegend.h>
 # include <TGraphErrors.h>
+# include <TMath.h>
+# include <TLine.h>
 # include <RAT/DS/Root.hh>
 # include <RAT/DS/MC.hh>
 # include <RAT/DS/MCTrack.hh>
@@ -24,214 +29,296 @@
 
 const double PI = 3.14159265358979323846;
 
+double sigmoid(double *x, double *params) {
+	return params[0]/(1+ TMath::Exp(-params[2]*(x[0]-params[1])));
+}
+
+double logistic_general(double *x, double *params) {
+	return params[0]/TMath::Power(1 + params[3]*TMath::Exp(-params[2]*(x[0]-params[1])), 1/params[4]);
+}
+
 int strToInt(char* ch) {
-  std::stringstream ss;
-  ss << ch; 
-  int num;
-  ss >> num; 
-  return num; 
+	std::stringstream ss;
+	ss << ch; 
+	int num;
+	ss >> num; 
+	return num; 
 }
 
 TVector3 getNormal(double angle) {
 
-  double x = 0.0; 
-  double y = cos(angle * PI/180); 
-  double z = sin(angle * PI/180); 
+	double x = 0.0; 
+	double y = cos(angle * PI/180); 
+	double z = sin(angle * PI/180); 
 
-  return TVector3(x, y, z); 
+	return TVector3(x, y, z); 
 }
 
 double photonTracker(std::string fname, int normalAngle, int sampleAngle, TFile* diagnostic) {
-  std::cout << fname << std::endl;
-  if (gSystem->AccessPathName(fname.c_str())) {
-    std::cout << "File " << fname << " does not exist, assuming 0 photons detected" << std::endl; 
-    return 0.0;
-  }
-  TFile* file = new TFile(fname.c_str(), "READ");
-  if (file->IsZombie()) {
-    std::cout << "File " << fname << " is zombie, will skip it." << std::endl;
-    throw std::bad_alloc();
-  }
-  TTree* tr = (TTree*) file->Get("T");
+	std::cout << fname << std::endl;
+	TFile* file = new TFile(fname.c_str(), "READ");
+	if (file->IsZombie()) {
+		std::cout << "File " << fname << " is zombie, will skip it." << std::endl;
+		throw std::bad_alloc();
+	}
+	TTree* tr = (TTree*) file->Get("T");
 
-  RAT::DS::Root* rds = new RAT::DS::Root();
-  tr->SetBranchAddress("ds", &rds);
+	RAT::DS::Root* rds = new RAT::DS::Root();
+	tr->SetBranchAddress("ds", &rds);
 
-  char upstreamName[1000];
-  sprintf(upstreamName, "upstreamAngle_%d_%d", sampleAngle, normalAngle);
-  char downstreamName[1000];
-  sprintf(downstreamName, "downstreamAngle_%d_%d", sampleAngle, normalAngle);
-  char positionName[1000];
-  sprintf(positionName, "photonPosition_%d_%d", sampleAngle, normalAngle);
-  char wavelengthName[1000];
-  sprintf(wavelengthName, "photonWavelength_%d_%d", sampleAngle, normalAngle);
-  TH1F* upstreamAngle = new TH1F(upstreamName, "Upstream Angle", 100, -1.1, 1.1);
-  TH1F* downstreamAngle = new TH1F(downstreamName, "Downstream Angle", 100, -1.1, 1.1);
-  TH2F* yzPositions = new TH2F(positionName, "Y-Z Positions", 100, -500, 500, 100, -100, 100); 
-  TH1F* wavelength = new TH1F(wavelengthName, "Wavelength", 100, 0, 400); 
-  
-  TVector3 hatX = TVector3(1.0,0,0);
-  TVector3 hatY = TVector3(0,1.0,0);
-  TVector3 hatZ = TVector3(0,0,1.0);
-  TVector3 hat45 = TVector3(0, 1, 1);
-  TVector3 hat60 = TVector3(0, 1, 1.73205080757);
-  TVector3 photodiodeNormal = getNormal(normalAngle); 
-  int numCube = 0;
-  int numPhotodiode = 0;
+	char upstreamName[1000];
+	sprintf(upstreamName, "upstreamAngle_%d_%d", sampleAngle, normalAngle);
+	char downstreamName[1000];
+	sprintf(downstreamName, "downstreamAngle_%d_%d", sampleAngle, normalAngle);
+	char positionName[1000];
+	sprintf(positionName, "photonPosition_%d_%d", sampleAngle, normalAngle);
+	char wavelengthName[1000];
+	sprintf(wavelengthName, "photonWavelength_%d_%d", sampleAngle, normalAngle);
+	TH1F* upstreamAngle = new TH1F(upstreamName, "Upstream Angle", 100, -1.1, 1.1);
+	TH1F* downstreamAngle = new TH1F(downstreamName, "Downstream Angle", 100, -1.1, 1.1);
+	TH2F* yzPositions = new TH2F(positionName, "Y-Z Positions", 100, -500, 500, 100, -100, 100); 
+	TH1F* wavelength = new TH1F(wavelengthName, "Wavelength", 100, 0, 400); 
 
-  int nentries = tr->GetEntries();
-  std::cout << "Num Entries: " << nentries << std::endl;
+	TVector3 hatX = TVector3(1.0,0,0);
+	TVector3 hatY = TVector3(0,1.0,0);
+	TVector3 hatZ = TVector3(0,0,1.0);
+	TVector3 hat45 = TVector3(0, 1, 1);
+	TVector3 hat60 = TVector3(0, 1, 1.73205080757);
+	TVector3 photodiodeNormal = getNormal(normalAngle); 
+	int numCube = 0;
+	int numPhotodiode = 0;
 
-  for (int iEntry = 0; iEntry < nentries; iEntry++) {
+	int nentries = tr->GetEntries();
+	std::cout << "Num Entries: " << nentries << std::endl;
 
-    if (iEntry % 1000 == 0) {
-      std::cout << "At entry " << iEntry << std::endl;
-    }
+	//for (int iEntry = 0; iEntry < nentries; iEntry++) {
+	for (int iEntry = 0; iEntry < 1000; iEntry++) {
 
-    tr->GetEntry(iEntry); 
-    RAT::DS::MC* rmc = rds->GetMC(); 
-    int numMCTracks = rmc->GetMCTrackCount(); 
+		if (iEntry % int(nentries/10) == 0) {
+			std::cout << "At entry " << iEntry << std::endl;
+		}
 
-    for (int iTrack = 0; iTrack < numMCTracks; iTrack++) {
+		tr->GetEntry(iEntry); 
+		RAT::DS::MC* rmc = rds->GetMC(); 
+		int numMCTracks = rmc->GetMCTrackCount(); 
 
-      RAT::DS::MCTrack* photonTrack = rmc->GetMCTrack(iTrack); 
-      int numSteps = photonTrack->GetMCTrackStepCount(); 
+		for (int iTrack = 0; iTrack < numMCTracks; iTrack++) {
 
-      for (int iStep = 0; iStep < numSteps; iStep++) {
+			RAT::DS::MCTrack* photonTrack = rmc->GetMCTrack(iTrack); 
+			int numSteps = photonTrack->GetMCTrackStepCount(); 
 
-        RAT::DS::MCTrackStep* photonStep = photonTrack->GetMCTrackStep(iStep);
-        double yComponent = photonStep->GetMomentum().Dot(hatY); 
+			for (int iStep = 0; iStep < numSteps; iStep++) {
 
-        double xPos = photonStep->GetEndpoint().x();
-        double yPos = photonStep->GetEndpoint().y();
-        double zPos = photonStep->GetEndpoint().z();
-        yzPositions->Fill(yPos, zPos);
+				RAT::DS::MCTrackStep* photonStep = photonTrack->GetMCTrackStep(iStep);
+				double yComponent = photonStep->GetMomentum().Dot(hatY); 
 
-        double photonWavelength = 1.2398/(photonStep->GetKE()*pow(10,6))*1000.; 
-        wavelength->Fill(photonWavelength); 
+				//double xPos = photonStep->GetEndpoint().x();
+				double yPos = photonStep->GetEndpoint().y();
+				double zPos = photonStep->GetEndpoint().z();
+				yzPositions->Fill(yPos, zPos);
 
-        if (photonStep->GetVolume() == "testdisk" && yComponent >= 0.0) {
-          numCube++; 
-          double cos_theta = (photonStep->GetMomentum().Dot(hatY))/(photonStep->GetMomentum().Mag());
-          upstreamAngle->Fill(cos_theta); 
-        } 
+				double photonWavelength = 1.2398/(photonStep->GetKE()*pow(10,6))*1000.; 
+				wavelength->Fill(photonWavelength); 
 
-        if (photonStep->GetVolume() == "verticalSurface" || photonStep->GetVolume() == "photodiodetest") {
-          numPhotodiode++; 
-          //       TVector3 photoDiodeNormal = hatY;//getNormal(normalAngle); 
-          RAT::DS::MCTrackStep* p = photonTrack->GetMCTrackStep(iStep-1);
-          RAT::DS::MCTrackStep* pp = photonTrack->GetMCTrackStep(iStep-2);
-          RAT::DS::MCTrackStep* ppp = photonTrack->GetMCTrackStep(iStep-3);
-          double cos_theta = (photonStep->GetMomentum().Dot(hatY))/(photonStep->GetMomentum().Mag() * hatY.Mag());
-          //std::cout << cos_theta << std::endl;
-          downstreamAngle->Fill(cos_theta); 
-        }
+				if (photonStep->GetVolume() == "testdisk" && yComponent >= 0.0) {
+					numCube++; 
+					double cos_theta = (photonStep->GetMomentum().Dot(hatY))/(photonStep->GetMomentum().Mag());
+					upstreamAngle->Fill(cos_theta); 
+				} 
 
-      }
+				if (photonStep->GetVolume() == "verticalSurface" || photonStep->GetVolume() == "photodiodetest") {
+					numPhotodiode++; 
+					//       TVector3 photoDiodeNormal = hatY;//getNormal(normalAngle); 
+					//RAT::DS::MCTrackStep* p = photonTrack->GetMCTrackStep(iStep-1);
+					//RAT::DS::MCTrackStep* pp = photonTrack->GetMCTrackStep(iStep-2);
+					//RAT::DS::MCTrackStep* ppp = photonTrack->GetMCTrackStep(iStep-3);
+					double cos_theta = (photonStep->GetMomentum().Dot(hatY))/(photonStep->GetMomentum().Mag() * hatY.Mag());
+					//std::cout << cos_theta << std::endl;
+					downstreamAngle->Fill(cos_theta); 
+				}
 
-    }
-  }
+			}
 
-  std::cout << "UPSTREAM NUMBER FOR ANGLE " << normalAngle << " " << numCube << std::endl;
-  std::cout << "NUMBER FOR ANGLE " << normalAngle << " " << numPhotodiode << std::endl;
-  diagnostic->WriteTObject(upstreamAngle);
-  diagnostic->WriteTObject(downstreamAngle); 
-  diagnostic->WriteTObject(yzPositions);
-  diagnostic->WriteTObject(wavelength);
-  diagnostic->WriteTObject(zDist); 
-  return double(numPhotodiode); 
+		}
+	}
+	delete rds; // clean up
 
+	std::cout << "UPSTREAM NUMBER FOR ANGLE " << normalAngle << " " << numCube << std::endl;
+	std::cout << "NUMBER FOR ANGLE " << normalAngle << " " << numPhotodiode << std::endl;
+	diagnostic->WriteTObject(upstreamAngle);
+	diagnostic->WriteTObject(downstreamAngle); 
+	diagnostic->WriteTObject(yzPositions);
+	diagnostic->WriteTObject(wavelength); 
+
+	delete upstreamAngle; // clean up
+	delete downstreamAngle;
+	delete yzPositions;
+	delete wavelength;
+
+	return double(numPhotodiode); 
 } 
 
-
-
 void newLoopOverFiles(int sampleMin, int sampleMax, int sampleDelta, 
-                      int normalMin, int normalMax, int normalDelta) {
+					  int normalMin, int normalMax, int normalDelta) {
 
-  gROOT->SetBatch(kTRUE); 
-  TFile* outf = new TFile("angular_sweeps.root", "RECREATE"); 
-  TMultiGraph* mg = new TMultiGraph();
-  auto leg = new TLegend(0.1,0.7,0.2,0.9); 
-  TFile* diagnosticAngle = new TFile("angularDiagnostics.root", "RECREATE");   
+	gROOT->SetBatch(kTRUE); 
+	TFile* outf = new TFile("angular_sweeps.root", "RECREATE"); 
+	TMultiGraph* mg = new TMultiGraph(); 
+	TFile* diagnosticAngle = new TFile("angularDiagnostics.root", "RECREATE");   
 
-  for (int sampleAngle = sampleMin; sampleAngle < sampleMax + sampleDelta; sampleAngle += sampleDelta) {
+	std::vector<double> intensityPeakHeights;
+	std::vector<double> intensityPeakHeights_err;
+	std::vector<double> sampleAngles;
 
-    int numDataPoints = (normalMax - normalMin)/normalDelta + 1;
-    std::cout << numDataPoints << std::endl;
-    std::vector<double> normalAngles; 
-    std::vector<double> intensities; 
-    std::vector<double> errors; 
+	unsigned int color = 1;
+	for (int sampleAngle = sampleMin; sampleAngle < sampleMax + sampleDelta; sampleAngle += sampleDelta) {
+		normalMin = sampleAngle - 5;
+		normalMax = sampleAngle + 5;
+		int numDataPoints = (normalMax - normalMin)/normalDelta + 1;
+		std::cout << numDataPoints << std::endl;
 
-    char histname[1000];
-    sprintf(histname, "angularDist_%d", sampleAngle); 
-    TH1F* hist = new TH1F(histname, "Photon Angular Distribution;Angle From Normal / Degrees;Number of Photons", 500, normalMin, normalMax + normalDelta); 
-    hist->SetLineWidth(2); 
-    for (int normalAngle = normalMin; normalAngle < normalMax + normalDelta; normalAngle += normalDelta) {
+		std::vector<double> normalAngles; 
+		std::vector<double> intensities; 
+		std::vector<double> errors; 
 
-      char filename[1000];
-      sprintf(filename, "./angle_sweep/angle_%d/no_cylinder_angle_%d.%d.root", sampleAngle, sampleAngle,normalAngle);
-      double photonCount = -1;
-      try {
-        photonCount = photonTracker(filename, normalAngle, sampleAngle, diagnosticAngle);
-      } catch (const std::exception& exc) {
-        std::cout << "Exception raised: " << exc.what() << std::endl;
-        TFile* file = dynamic_cast<TFile*>(gROOT->GetListOfFiles()->FindObject(filename)); // clean up zombie ROOT file that was created in photonTracker() method
-        delete file;
-        continue;
-      }
+		char histname[1000];
+		sprintf(histname, "angularDist_%d", sampleAngle); 
+		TH1F* hist = new TH1F(histname, "Photon Angular Distribution;Angle From Normal / Degrees;Number of Photons", 500, normalMin, normalMax + normalDelta); 
+		hist->SetLineWidth(2); 
+		for (int normalAngle = normalMin; normalAngle < normalMax + normalDelta; normalAngle += normalDelta) {
+			char filename[1000];
+			sprintf(filename, "./angle_sweep/angle_%d/no_cylinder_angle_%d.%d.root", sampleAngle, sampleAngle,normalAngle);
+			double photonCount = -1; // initialize to nonsense value
+			try {
+				photonCount = photonTracker(filename, normalAngle, sampleAngle, diagnosticAngle);
+			} catch (const std::exception& exc) {
+				std::cout << "Exception raised: " << exc.what() << std::endl;
+				TFile* file = dynamic_cast<TFile*>(gROOT->GetListOfFiles()->FindObject(filename)); // clean up zombie ROOT file that was created in photonTracker() method
+				delete file;
+				continue;
+			}
 
-      normalAngles.push_back(normalAngle); 
-      intensities.push_back(photonCount); 
-      errors.push_back(sqrt(photonCount));      
+			normalAngles.push_back(normalAngle); 
+			intensities.push_back(photonCount); 
+			errors.push_back(sqrt(photonCount));      
 
-      hist->Fill(normalAngle, photonCount); 
+			hist->Fill(normalAngle, photonCount); 
+		}
+		assert(normalAngles.size() == intensities.size()); // sanity checks
+		assert(normalAngles.size() == errors.size());
 
-    }
+		if (intensities.size() == 0) {
+			std::cout << "Could not read any photon intensities. Are you setting the correct PMT viewing angles to read?" << std::endl;
+			return;
+		}
 
-    double* normalArray = &normalAngles[0]; 
-    double* intensityArray = &intensities[0]; 
-    double* errorArray = &errors[0]; 
+		// Skip colors I don't like
+		color = color % 10;
+		if (color % 10 == 0) {
+			color++;
+		}
 
-    TGraphErrors* gr = new TGraphErrors(normalAngles.size(), normalArray, intensityArray, 0, errorArray); 
-    gr->SetMarkerStyle(21);
-    gr->SetLineWidth(2);
-    gr->SetLineStyle(9);
-    gr->SetLineColor(sampleAngle); 
-    gr->SetLineColor((sampleAngle - sampleMin) % 10); 
-    gr->SetMarkerColor((sampleAngle - sampleMin) % 10); 
-    gr->GetXaxis()->SetTitle("Angle from Normal / degrees");
-    gr->GetYaxis()->SetTitle("Number Of Photons");
+		sampleAngles.push_back(sampleAngle);
+		const double maxIntensity = *std::max_element(intensities.begin(), intensities.end());
+		intensityPeakHeights.push_back(maxIntensity);
+		intensityPeakHeights_err.push_back(sqrt(maxIntensity));
 
-    gr->SetTitle(histname); 
-    gr->SetName(histname);  
-    //outf->WriteTObject(gr); 
-    outf->WriteTObject(hist); 
-    mg->Add(gr); 
-  } 
+		double* normalArray = &normalAngles[0]; 
+		double* intensityArray = &intensities[0]; 
+		double* errorArray = &errors[0]; 
 
-  std::cout << "LOOP COMPLETE" << std::endl;
-  mg->SetName("distribution_multigraph");  
-  outf->WriteTObject(mg); 
-  TCanvas* canv = new TCanvas("c", "c", 1000, 600);
-  mg->GetXaxis()->SetTitle("Angle from Normal / degrees"); 
-  mg->GetYaxis()->SetTitle("Normalised Intensity"); 
-  mg->Draw("apc");
-  mg->GetXaxis()->SetTitle("Angle from Normal / Degrees");
-  mg->GetYaxis()->SetTitle("Number of Photons");
-  canv->BuildLegend();
-  outf->WriteTObject(canv);
+		TGraphErrors* gr = new TGraphErrors(normalAngles.size(), normalArray, intensityArray, 0, errorArray); 
+		gr->SetMarkerStyle(21);
+		gr->SetLineWidth(2);
+		gr->SetLineStyle(9);
+		gr->SetMarkerColor(color); 
+		gr->SetLineColor(color); 
+		gr->SetFillStyle(0);
+		gr->GetXaxis()->SetTitle("Angle from Normal / degrees");
+		gr->GetYaxis()->SetTitle("Number Of Photons");
 
-std::cout << "COMPLETE!" << std::endl;
+		gr->SetTitle(histname); 
+		gr->SetName(histname);  
+		//outf->WriteTObject(gr); 
+		outf->WriteTObject(hist); 
+		mg->Add(gr); 
 
+		color++;
+	} 
+	assert(sampleAngles.size() == intensityPeakHeights.size()); // sanity checks
+	assert(sampleAngles.size() == intensityPeakHeights_err.size());
+	std::cout << "LOOP COMPLETE" << std::endl;
+
+	{ // Plot peak shapes vs incidence angle
+		mg->SetName("multigraph_peakShapesVsIncidenceAngle");  
+		outf->WriteTObject(mg); 
+		TCanvas canvas_0("canvas_peakShapesVsIncidenceAngle", "canvas_peakShapesVsIncidenceAngle", 1000, 600);
+		mg->Draw("a:p:l");
+		mg->GetXaxis()->SetTitle("Angle from Normal [degree]");
+		mg->GetYaxis()->SetTitle("Number of Photons");
+		mg->GetXaxis()->SetLimits(sampleMin - 10, sampleMax + 10);
+		canvas_0.BuildLegend();
+		canvas_0.Modified();
+		canvas_0.Update();
+		outf->WriteTObject(&canvas_0);
+	}
+
+	{ // Plot peak heights vs incidence angle
+		TGraphErrors* peakHeightVsIncidenceAngle = new TGraphErrors(intensityPeakHeights.size(), &sampleAngles[0], &intensityPeakHeights[0], NULL, &intensityPeakHeights_err[0]);
+		peakHeightVsIncidenceAngle->SetTitle("");
+		peakHeightVsIncidenceAngle->SetName("graph_peakHeightVsIncidenceAngle");
+
+		// Draw
+		TCanvas canvas_1("canvas_peakHeightVsIncidenceAngle", "canvas_peakHeightVsIncidenceAngle");
+		canvas_1.Draw();
+		canvas_1.cd();
+		peakHeightVsIncidenceAngle->SetMarkerStyle(8);
+		peakHeightVsIncidenceAngle->Draw("a:p");
+		peakHeightVsIncidenceAngle->GetXaxis()->SetTitle("Nominal beam incidence angle [degree]");
+		peakHeightVsIncidenceAngle->GetYaxis()->SetTitle("Number of photons");
+
+		// Fit with function
+		TF1 fitFunction("fitFunction", &logistic_general, sampleMin, sampleMax, 5);
+		fitFunction.SetParNames("amplitude", "location", "scale", "Q", "nu");
+		double amplitude_init, junk;
+		peakHeightVsIncidenceAngle->GetPoint(0, junk, amplitude_init);
+		fitFunction.SetParameters(amplitude_init, 0.5*(sampleMin + sampleMax), -1, 1, 1);
+		peakHeightVsIncidenceAngle->Fit("fitFunction", "R"); // do fit
+
+		//// Find threshold at 90 % of maximum intensity
+		//double amplitude = fitFunction.GetParameter(0);
+		//double y_threshold = 0.9*amplitude;
+		//double x_threshold = fitFunction.GetX(y_threshold, sampleMin, sampleMax);
+		//std::cout << "x_threshold: " << x_threshold - 180 << ";" << std::endl;
+		//TLine threshold_horizontal(peakHeightVsIncidenceAngle->GetXaxis()->GetXmin(), y_threshold, x_threshold, y_threshold);
+		//threshold_horizontal.SetLineStyle(2);
+		//threshold_horizontal.Draw("Same");
+		//TLine threshold_vertical(x_threshold, peakHeightVsIncidenceAngle->GetYaxis()->GetXmin(), x_threshold, y_threshold);
+		//threshold_vertical.SetLineStyle(2);
+		//threshold_vertical.Draw("Same");
+		//canvas_1.Modified();
+		//canvas_1.Update();
+
+		// Save fitted function
+		outf->WriteTObject(peakHeightVsIncidenceAngle);
+		outf->WriteTObject(&canvas_1);
+		outf->Close();
+	}
+
+	std::cout << "COMPLETE!" << std::endl;
+
+	delete outf;
+	delete peakHeightVsIncidenceAngle;
+	delete diagnosticAngle;
 }
 
 int main(int argc, char* argv[]) {
-  int samplemin = strToInt(argv[1]);
-  int samplemax = strToInt(argv[2]); 
-  int sampledelta = strToInt(argv[3]); 
-  int normalmin = strToInt(argv[4]);
-  int normalmax = strToInt(argv[5]); 
-  int normaldelta = strToInt(argv[6]);  
-  newLoopOverFiles(samplemin, samplemax, sampledelta, normalmin, normalmax, normaldelta); 
-  return 0; 
+	int samplemin = strToInt(argv[1]);
+	int samplemax = strToInt(argv[2]); 
+	int sampledelta = strToInt(argv[3]); 
+	int normalmin = strToInt(argv[4]);
+	int normalmax = strToInt(argv[5]); 
+	int normaldelta = strToInt(argv[6]);  
+	newLoopOverFiles(samplemin, samplemax, sampledelta, normalmin, normalmax, normaldelta); 
+	return 0; 
 }

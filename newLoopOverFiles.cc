c490aad058a44e76b28a58e153ef37d140d555c8
Michinari Sakai <michsakai@gmail.com>
2019-04-12 18:40:16 -0700

    Add feature to plot reflected peak angle vs nominal incidence angle. Also clean up some names to be more explicitly clear for various plots because now we have a few.

--- newLoopOverFiles.cc
+++ newLoopOverFiles.cc
@@ -27,14 +27,16 @@
 # include <RAT/DS/MCTrackStep.hh>
 # include <TSystem.h>
 
-const double PI = 3.14159265358979323846;
-
 double sigmoid(double *x, double *params) {
 	return params[0]/(1+ TMath::Exp(-params[2]*(x[0]-params[1])));
 }
 
 double logistic_general(double *x, double *params) {
 	return params[0]/TMath::Power(1 + params[3]*TMath::Exp(-params[2]*(x[0]-params[1])), 1/params[4]);
+}
+
+double linear(double *x, double *params) {
+	return params[0]*x[0] + params[1];
 }
 
 int strToInt(char* ch) {
@@ -48,8 +50,8 @@ int strToInt(char* ch) {
 TVector3 getNormal(double angle) {
 
 	double x = 0.0; 
-	double y = cos(angle * PI/180); 
-	double z = sin(angle * PI/180); 
+	double y = cos(angle * TMath::Pi()/180); 
+	double z = sin(angle * TMath::Pi()/180); 
 
 	return TVector3(x, y, z); 
 }
@@ -91,7 +93,8 @@ double photonTracker(std::string fname, int normalAngle, int sampleAngle, TFile*
 	int nentries = tr->GetEntries();
 	std::cout << "Num Entries: " << nentries << std::endl;
 
-	for (int iEntry = 0; iEntry < nentries; iEntry++) {
+	//for (int iEntry = 0; iEntry < nentries; iEntry++) { // FIX ME
+	for (int iEntry = 0; iEntry < 1000; iEntry++) {
 
 		if (iEntry % int(nentries/10) == 0) {
 			std::cout << "At entry " << iEntry << std::endl;
@@ -167,7 +170,8 @@ void newLoopOverFiles(int sampleMin, int sampleMax, int sampleDelta,
 
 	std::vector<double> intensityPeakHeights;
 	std::vector<double> intensityPeakHeights_err;
-	std::vector<double> sampleAngles;
+	std::vector<double> reflectedPeakAngles;
+	std::vector<double> nominalIncidenceAngles;
 
 	unsigned int color = 1;
 	for (int sampleAngle = sampleMin; sampleAngle < sampleMax + sampleDelta; sampleAngle += sampleDelta) {
@@ -217,7 +221,8 @@ void newLoopOverFiles(int sampleMin, int sampleMax, int sampleDelta,
 			color++;
 		}
 
-		sampleAngles.push_back(sampleAngle);
+		nominalIncidenceAngles.push_back(sampleAngle);
+		reflectedPeakAngles.push_back(normalAngles.at(std::distance(intensities.begin(), std::max_element(intensities.begin(), intensities.end()))));
 		const double maxIntensity = *std::max_element(intensities.begin(), intensities.end());
 		intensityPeakHeights.push_back(maxIntensity);
 		intensityPeakHeights_err.push_back(sqrt(maxIntensity));
@@ -244,69 +249,133 @@ void newLoopOverFiles(int sampleMin, int sampleMax, int sampleDelta,
 
 		color++;
 	} 
-	assert(sampleAngles.size() == intensityPeakHeights.size()); // sanity checks
-	assert(sampleAngles.size() == intensityPeakHeights_err.size());
 	std::cout << "LOOP COMPLETE" << std::endl;
 
 	{ // Plot peak shapes vs incidence angle
-		mg->SetName("multigraph_peakShapesVsIncidenceAngle");  
-		outf->WriteTObject(mg); 
-		TCanvas canvas_0("canvas_peakShapesVsIncidenceAngle", "canvas_peakShapesVsIncidenceAngle", 1000, 600);
+		mg->SetName("multigraph_reflectedPeakShapeVsNominalIncidenceAngle");  
+		TCanvas canvas_0("canvas_reflectedPeakShapeVsNominalIncidenceAngle", "canvas_reflectedPeakShapeVsNominalIncidenceAngle", 1000, 600);
 		mg->Draw("a:p:l");
 		mg->GetXaxis()->SetTitle("Angle from Normal [degree]");
 		mg->GetYaxis()->SetTitle("Number of Photons");
 		mg->GetXaxis()->SetLimits(sampleMin - 10, sampleMax + 10);
-		canvas_0.BuildLegend();
+		TLegend* legend = canvas_0.BuildLegend(0.15, 0.15, 0.6, 0.7);
+		legend->SetFillColor(kWhite);
 		canvas_0.Modified();
 		canvas_0.Update();
+
+		outf->WriteTObject(mg); 
 		outf->WriteTObject(&canvas_0);
 	}
 
 	{ // Plot peak heights vs incidence angle
-		TGraphErrors* peakHeightVsIncidenceAngle = new TGraphErrors(intensityPeakHeights.size(), &sampleAngles[0], &intensityPeakHeights[0], NULL, &intensityPeakHeights_err[0]);
-		peakHeightVsIncidenceAngle->SetTitle("");
-		peakHeightVsIncidenceAngle->SetName("graph_peakHeightVsIncidenceAngle");
+		assert(nominalIncidenceAngles.size() == intensityPeakHeights.size()); // sanity check
+		assert(nominalIncidenceAngles.size() == intensityPeakHeights_err.size()); // sanity check
+
+		TGraphErrors* reflectedPeakHeightVsNominalIncidenceAngle = new TGraphErrors(intensityPeakHeights.size(), &nominalIncidenceAngles[0], &intensityPeakHeights[0], NULL, &intensityPeakHeights_err[0]);
+		reflectedPeakHeightVsNominalIncidenceAngle->SetTitle("");
+		reflectedPeakHeightVsNominalIncidenceAngle->SetName("graph_reflectedPeakHeightVsNominalIncidenceAngle");
 
 		// Draw
-		TCanvas canvas_1("canvas_peakHeightVsIncidenceAngle", "canvas_peakHeightVsIncidenceAngle");
+		TCanvas canvas_1("canvas_reflectedPeakHeightVsNominalIncidenceAngle", "canvas_reflectedPeakHeightVsNominalIncidenceAngle");
 		canvas_1.Draw();
 		canvas_1.cd();
-		peakHeightVsIncidenceAngle->SetMarkerStyle(8);
-		peakHeightVsIncidenceAngle->Draw("a:p");
-		peakHeightVsIncidenceAngle->GetXaxis()->SetTitle("Nominal beam incidence angle [degree]");
-		peakHeightVsIncidenceAngle->GetYaxis()->SetTitle("Number of photons");
+		reflectedPeakHeightVsNominalIncidenceAngle->SetMarkerStyle(8);
+		reflectedPeakHeightVsNominalIncidenceAngle->Draw("a:p");
+		reflectedPeakHeightVsNominalIncidenceAngle->GetXaxis()->SetTitle("Nominal beam incidence angle [degree]");
+		reflectedPeakHeightVsNominalIncidenceAngle->GetYaxis()->SetTitle("Number of photons");
 
 		// Fit with function
 		TF1 fitFunction("fitFunction", &logistic_general, sampleMin, sampleMax, 5);
 		fitFunction.SetParNames("amplitude", "location", "scale", "Q", "nu");
 		double amplitude_init, junk;
-		peakHeightVsIncidenceAngle->GetPoint(0, junk, amplitude_init);
+		reflectedPeakHeightVsNominalIncidenceAngle->GetPoint(0, junk, amplitude_init);
 		fitFunction.SetParameters(amplitude_init, 0.5*(sampleMin + sampleMax), -1, 1, 1);
-		peakHeightVsIncidenceAngle->Fit("fitFunction", "R"); // do fit
+		reflectedPeakHeightVsNominalIncidenceAngle->Fit("fitFunction", "R"); // do fit
 
 		//// Find threshold at 90 % of maximum intensity
 		//double amplitude = fitFunction.GetParameter(0);
 		//double y_threshold = 0.9*amplitude;
 		//double x_threshold = fitFunction.GetX(y_threshold, sampleMin, sampleMax);
 		//std::cout << "x_threshold: " << x_threshold - 180 << ";" << std::endl;
-		//TLine threshold_horizontal(peakHeightVsIncidenceAngle->GetXaxis()->GetXmin(), y_threshold, x_threshold, y_threshold);
+		//TLine threshold_horizontal(reflectedPeakHeightVsNominalIncidenceAngle->GetXaxis()->GetXmin(), y_threshold, x_threshold, y_threshold);
 		//threshold_horizontal.SetLineStyle(2);
 		//threshold_horizontal.Draw("Same");
-		//TLine threshold_vertical(x_threshold, peakHeightVsIncidenceAngle->GetYaxis()->GetXmin(), x_threshold, y_threshold);
+		//TLine threshold_vertical(x_threshold, reflectedPeakHeightVsNominalIncidenceAngle->GetYaxis()->GetXmin(), x_threshold, y_threshold);
 		//threshold_vertical.SetLineStyle(2);
 		//threshold_vertical.Draw("Same");
 		//canvas_1.Modified();
 		//canvas_1.Update();
 
 		// Save fitted function
-		outf->WriteTObject(peakHeightVsIncidenceAngle);
+		outf->WriteTObject(reflectedPeakHeightVsNominalIncidenceAngle);
 		outf->WriteTObject(&canvas_1);
-		outf->Close();
+	}
 
-		delete peakHeightVsIncidenceAngle;
+	{ // Plot reflected peak angle vs nominal incidence angle
+		assert(nominalIncidenceAngles.size() == reflectedPeakAngles.size()); // sanity check
+
+		TGraph* reflectedPeakAngleVsNominalIncidenceAngle = new TGraph(reflectedPeakAngles.size(), &nominalIncidenceAngles[0], &reflectedPeakAngles[0]);
+		reflectedPeakAngleVsNominalIncidenceAngle->SetTitle("");
+		reflectedPeakAngleVsNominalIncidenceAngle->SetName("graph_reflectedPeakAngleVsNominalIncidenceAngle");
+
+		// Canvas
+		TCanvas canvas("canvas_reflectedPeakAngleVsNominalIncidenceAngle", "canvas_reflectedPeakAngleVsNominalIncidenceAngle");
+		canvas.Draw();
+		canvas.cd();
+		reflectedPeakAngleVsNominalIncidenceAngle->Draw("A:P");
+
+		// Draw y = x linear line, and uncertainty band.
+		// Band
+		std::vector<double> xs_band{0, reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax()};
+		std::vector<double> ys_band_upper{1.5, reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax() + 1.5};
+		std::vector<double> ys_band_lower{-1.5, reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax() + -1.5};
+		assert(xs_band.size() == ys_band_upper.size());
+		assert(xs_band.size() == ys_band_lower.size());
+		TGraph band_upper(xs_band.size(), &xs_band[0], &ys_band_upper[0]);
+		TGraph band_lower(xs_band.size(), &xs_band[0], &ys_band_lower[0]);
+		TGraph band(2*xs_band.size());
+		for (size_t i = 0; i < xs_band.size(); i++) {
+			band.SetPoint(i, xs_band[i], ys_band_upper[i]);
+			band.SetPoint(xs_band.size() + i, xs_band[xs_band.size() - i - 1], ys_band_lower[xs_band.size() - i - 1]);
+		}
+		band.SetFillStyle(1001);
+		band.SetFillColor(17);
+		band.Draw("F");
+		// Line
+		TLine linearLine(0, 0, reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax(), reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax());
+		linearLine.SetLineStyle(2);
+		linearLine.Draw("");
+		canvas.Modified();
+		canvas.Update();
+
+		// Fit data
+		std::cout << "sampleMin" << sampleMin << ";" << std::endl;
+		std::cout << "sampleMax" << sampleMax << ";" << std::endl;
+		TF1 fitFunction("fitFunction", &linear, sampleMin, sampleMax, 2);
+		fitFunction.SetParNames("slope", "y-intercept");
+		fitFunction.SetParameters(1, 0);
+		std::cout << "reflectedPeakAngleVsNominalIncidenceAngle->GetN()" << reflectedPeakAngleVsNominalIncidenceAngle->GetN() << ";" << std::endl;
+		reflectedPeakAngleVsNominalIncidenceAngle->Fit(&fitFunction, "R"); // do fit
+
+		// Draw data
+		reflectedPeakAngleVsNominalIncidenceAngle->SetMarkerStyle(8);
+		reflectedPeakAngleVsNominalIncidenceAngle->Draw("P:Same");
+		//reflectedPeakAngleVsNominalIncidenceAngle->Draw("A:P");
+		reflectedPeakAngleVsNominalIncidenceAngle->SetMinimum(0);
+		reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->SetLimits(0, reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->GetXmax());
+		reflectedPeakAngleVsNominalIncidenceAngle->GetXaxis()->SetTitle("Nominal beam incidence angle [degree]");
+		reflectedPeakAngleVsNominalIncidenceAngle->GetYaxis()->SetTitle("Reflected beam peak angle [degree]");
+		canvas.Modified();
+		canvas.Update();
+
+		// Save graph and canvas
+		outf->WriteTObject(reflectedPeakAngleVsNominalIncidenceAngle);
+		outf->WriteTObject(&canvas);
 	}
 
 	std::cout << "COMPLETE!" << std::endl;
+
+	outf->Close();
 
 	delete outf;
 	delete diagnosticAngle;
